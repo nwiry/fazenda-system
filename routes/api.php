dce0f7c7775415f8b1735334f54e0b4332c780d8
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-01-05 16:08:44 -0300

    Rotas API para executar as funções do sistema adicionadas

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Cattle;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,3 +14,25 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+/**
+ * @version 1.0 - Primeira versão da API do Sistema
+ */
+Route::prefix('v1')->group(function () {
+    /**
+     * @api /api/v1/edit_cattle - Chama a função para editar o gado retornando a resposta da execução
+     */
+    Route::post('/edit_cattle', [Cattle::class, 'edit']);
+    /**
+     * @api /api/v1/list_cattle - Chama a função para listar os gados cadastrados retornando uma lista com os dados
+     */
+    Route::get('/list_cattle', [Cattle::class, 'list']);
+    /**
+     * @api /api/v1/list_slaughters - Chama a função para listar os animais disponíeis para abate retornando uma lista com os dados
+     */
+    Route::get('/list_slaughters', [Cattle::class, 'slaughters']);
+    /**
+     * @api /api/v1/register_cattle - Chama a função para cadastrar o gado retornando a resposta da execução
+     */
+    Route::post('/register_cattle', [Cattle::class, 'register']);
+});
