0d6163fa0a1e132dc1eac2d19cea8be0e81b6f4a
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-01-05 18:17:12 -0300

    Variavel para apresentar as quantidades pela model adicionada

--- app/Http/Controllers/FarmPages.php
+++ app/Http/Controllers/FarmPages.php
@@ -2,12 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Cattle;
 use Illuminate\Http\Request;
 
 class FarmPages extends Controller
 {
     public static function dashboard()
     {
-        return view('dashboard');
+        return view('dashboard', ['nums_cattle' => Cattle::sum_cattles()]);
     }
 }
