325d3b1254bfb0b3dc8ec1ce956def5146b29891
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-01-05 13:39:32 -0300

    Função para cadastrar adicionada

---
+++ app/Models/Cattle.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class Cattle extends Model
+{
+    use HasFactory;
+
+    /**
+     * @param \Illuminate\Http\Request $request Obtem as requisições do HTTP para tratamento
+     * @return array Retorna um array com o resultado da ação
+     */
+    public static function register(Request $request): array
+    {
+        try {
+            DB::table('farm_cattle')->insert([
+                'code' => $request->input('cattle_code'),
+                'milk' => $request->input('cattle_milk'),
+                'feed' => $request->input('cattle_feed'),
+                'weight' => $request->input('cattle_weight'),
+                'birthday' => $request->input('cattle_birthday'),
+            ]);
+            return ['status' => 'success'];
+        } catch (\Illuminate\Database\QueryException $e) {
+            return ['status' => 'warning', 'response' => $e->getMessage()];
+        }
+    }
+}
