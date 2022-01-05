8ac2279c22f03365983d0f9a54f339018b1af1c6
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-01-05 17:10:42 -0300

    Conteudo do dashboard adicionado

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('base')
+
+@section('conteudo')
+<div class="col-lg-12">
+    <div class="card">
+        <h5 class="card-header">Relatórios</h5>
+        <div class="card-body">
+            <ul>
+                <li>Quantidade de leite produzida por semana: X</li>
+                <li>Número de animais abatidos: X</li>
+                <li>Número de ração necessária por semana: X</li>
+            </ul>
+        </div>
+    </div>
+</div>
+@endsection
