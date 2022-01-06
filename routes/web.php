f14dfa14a42db40b9f92bb00319da2f51916f858
Diogo Cerqueira <diogodeveloper@hotmail.com>
2022-01-05 21:00:12 -0300

    Rota de listagem de animais adicionada

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,5 @@ use Illuminate\Support\Facades\Route;
 Route::any('/', [FarmPages::class, 'dashboard']);
 // Cadastro de Animal
 Route::any('/new_cattle', [FarmPages::class, 'new_cattle']);
+// Listagem de Animais
+Route::any('/list_cattle', [FarmPages::class, 'list_cattle']);
